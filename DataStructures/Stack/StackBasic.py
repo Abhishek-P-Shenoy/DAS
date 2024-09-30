d9545450d71ca0eedb3f26dc8c9800ace7ff5cc0
AbGuru <116103254+Abhishek-P-Shenoy@users.noreply.github.com>
2024-09-30 22:01:00 +0530

    Update StackBasic.py
    pop operation in stack

--- DataStructures/Stack/StackBasic.py
+++ DataStructures/Stack/StackBasic.py
@@ -2,9 +2,14 @@
 
 stack1 = []
 
-""" this is to push value in stack """
+""" To push value in stack """
 
 stack1.append(1)
 stack1.append(2)
 stack1.append(3)
 print(stack1)
+
+""" To Pop value from a stack"""
+print(stack1.pop())
+print(stack1)
+
